8e30b9c2ba5e29c3155adfab2124def4a745be6f
vilipche <filipsotiroski@gmail.com>
2022-03-17 14:05:31 +0100

    Updates on the upload

--- PartA3_SotiroskiRifat.py
+++ PartA3_SotiroskiRifat.py
@@ -14,22 +14,24 @@ Adding the affiliations
 
 def load_affiliations(conn):
     print("Load affiliations")
-    query_string = ''' load CSV FROM 'file:////home/filip/Documents/UPC/SDM/SDM_Property_Graph/Data/university.csv' AS row FIELDTERMINATOR ','
-    CREATE (n:university {uni_id:row[0],name:row[1]}) return n'''
+    query_string = '''load CSV WITH HEADERS FROM 'file:///university.csv' AS row FIELDTERMINATOR ','
+                        CREATE (n:university {uni_id:row.`uni:ID`,name:row.`name:string[]`}) return n'''
     conn.query(query_string, db='dblp')
 
-    query_string = ''' load CSV FROM 'file:////home/filip/Documents/UPC/SDM/SDM_Property_Graph/Data/company.csv' AS row FIELDTERMINATOR ','
-    CREATE (n:company {com_id:row[0],name:row[1]}) return n'''
+    query_string = ''' load CSV WITH HEADERS FROM 'file:///company.csv' AS row FIELDTERMINATOR ',' 
+                         CREATE (n:company {com_id:row.`com:ID`,name:row.`name:string[]`}) return n'''
     conn.query(query_string, db='dblp')
 
-    query_string = ''' load CSV FROM 'file:////home/filip/Documents/UPC/SDM/SDM_Property_Graph/Data/affiliation_company.csv' AS row FIELDTERMINATOR ','
-    match (a:authors),(c:company) where (a.author_id)=row[0] and (c.com_id)=row[1] create (a)-[:affiliated_withc]->(c)'''
+    query_string = ''' load CSV WITH HEADERS FROM 'file:///affiliation_company.csv' AS row FIELDTERMINATOR ',' 
+                        match (a:authors),(c:company) 
+                        where (a.author_id)=toInteger(row.`:START_ID`) and (c.com_id)=row.`:END_ID` 
+                        create (a)-[:affiliated_withc]->(c)'''
     conn.query(query_string, db='dblp')
 
     query_string = ''' 
-    load CSV FROM 'file:////home/filip/Documents/UPC/SDM/SDM_Property_Graph/Data/affiliation_university.csv' AS row FIELDTERMINATOR ','
+    load CSV WITH HEADERS FROM 'file:///affiliation_university.csv' AS row FIELDTERMINATOR ','
     match (a:authors),(u:university) 
-    where (a.author_id)=row[0] and (u.uni_id)=row[1] 
+    where (a.author_id)=toInteger(row.`:START_ID`) and (u.uni_id)=row.`:END_ID`
     create (a)-[:affiliated_withu]->(u)
     '''
     conn.query(query_string, db='dblp')
@@ -41,12 +43,12 @@ Updating the reviews and changing the porperties
 def add_properties(conn):
     print("Adding properties")
     query_suggest = """
-                    MATCH(a: authors)-[r: reviewedby]-(ar:articles)
+                    MATCH(a: authors)-[r:reviewedby]-(ar:articles)
                     SET r.suggest = rand() < 0.66
                     """
     conn.query(query_suggest, db='dblp')
     query_article = """
-                    MATCH(a: authors)-[r: reviewedby]-(ar:articles)
+                    MATCH(a: authors)-[r:reviewedby]-(ar:articles)
                     WITH ar, collect(r.suggest)
                     AS suggestions
                     WITH ar, size([x IN suggestions WHERE x = true]) AS rev, size(suggestions) as sizesug
