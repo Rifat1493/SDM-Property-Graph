fd7b088836227cc20110e9a1e76d1348d8cd13d2
vilipche <filipsotiroski@gmail.com>
2022-03-20 21:02:19 +0100

    step 3 part D

--- PartD_SotiroskiRifat.py
+++ PartD_SotiroskiRifat.py
@@ -33,3 +33,38 @@ order by conf, percentage desc
 with conf, collect(com)[0] as final_community
 set conf.community = final_community
 
+3.
+// pagerannk for 6
+
+CALL gds.graph.create.cypher(
+  'pageRank',
+  'MATCH (a:articles {community: 6})RETURN id(a) as id',
+  'MATCH (a1:articles {community:6})-[r:cited_by]->(a2:articles {community: 6}) RETURN id(a1) as source, id(a2) as target'
+)
+YIELD
+  graphName AS graph, nodeQuery, nodeCount AS nodes, relationshipQuery, relationshipCount AS rels
+  
+  
+CALL gds.pageRank.stream('a')
+YIELD nodeId, score
+RETURN gds.util.asNode(nodeId) AS name, score as pageRank
+ORDER BY pageRank DESC, name ASC  
+  
+
+CALL gds.pageRank.write('a', {
+  maxIterations: 20,
+  dampingFactor: 0.85,
+  writeProperty: 'pagerank'
+})
+YIELD nodePropertiesWritten, ranIterations
+
+// change to 100 creating top 100 for each conference/community
+MATCH (a:articles)-[r:published_inc]->(c:conferences)
+WHERE a.community=6 AND c.community=a.community
+WITH c.conference as conf, a.article as art, a.pagerank as part
+ORDER BY conf, part  desc
+RETURN conf, collect(art)[0..3]
+
+
+
+  
